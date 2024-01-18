d03d16962acbeb07274a961cc8da4133fe151a5f
Mateus A. Kreuch <mateus.kreuch@gmail.com>
2024-01-18 01:16:55 -0300

    corrected math

--- shaders/common/math.glsl
+++ shaders/common/math.glsl
@@ -1,9 +1,17 @@
-#define rescale(x, a, b) clamp((x - a) / (b - a), 0.0, 1.0)
-#define squaredLength(v) dot(v, v)
-#define luma(color) dot(vec3(0.299, 0.587, 0.114), color)
-
 float noise(vec2 pos) {
 	return fract(sin(dot(pos, vec2(18.9898, 28.633))) * 4378.5453);
+}
+
+float luma(vec3 color) {
+   return dot(vec3(0.299, 0.587, 0.114), color);
+}
+
+float rescale(float x, float a, float b) {
+   return clamp((x - a) / (b - a), 0.0, 1.0);
+}
+
+float squaredLength(vec3 v) {
+   return dot(v, v);
 }
 
 vec3 nvec3(vec4 pos) {
