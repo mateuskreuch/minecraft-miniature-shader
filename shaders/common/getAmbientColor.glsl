1c82db419cb414616049614e360a97452c974c50
Mateus A. Kreuch <mateus.kreuch@gmail.com>
2026-07-24 17:15:45 -0300

    fixed ambient color in other dimensions

--- shaders/common/getAmbientColor.glsl
+++ shaders/common/getAmbientColor.glsl
@@ -1,16 +1,18 @@
 vec4 getAmbientColor(float skyLight, float sunHeight) {
    vec4 ambient = texture2D(lightmap, vec2(AMBIENT_UV.s, skyLight));
 
-   float x = ambient.g;
-   float final = (((0.8494 * x + 0.9687) * x - 5.238) * x + 3.711) * x - 0.2864;
+   #ifdef OVERWORLD
+      float x = ambient.g;
+      float final = (((0.8494 * x + 0.9687) * x - 5.238) * x + 3.711) * x - 0.2864;
 
-   final = max(0.0, final);
+      final = max(0.0, final);
 
-   ambient.rgb = mix(
-      min(vec3(1.0), ambient.rgb + vec3(final) * clamp(0.2*sunHeight, 0.0, 1.0)),
-      ambient.rgb,
-      screenBrightness
-   );
+      ambient.rgb = mix(
+         min(vec3(1.0), ambient.rgb + vec3(final) * clamp(0.2*sunHeight, 0.0, 1.0)),
+         ambient.rgb,
+         screenBrightness
+      );
+   #endif
 
    return ambient;
 }
